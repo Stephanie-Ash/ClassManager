676a58615d1dda987ddd6d2ef54c47fc052e5743
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-11-22 12:11:47 +0000

    Add initial homepage layout

--- src/main/java/com/qa/classmanager/controller/PageController.java
+++ src/main/java/com/qa/classmanager/controller/PageController.java
@@ -10,26 +10,31 @@ import com.qa.classmanager.persistence.domain.Student;
 import com.qa.classmanager.service.StudentService;
 
 @Controller
-public class StudentController {
+public class PageController {
 	
 	private StudentService service;
 	
-	public StudentController(StudentService service) {
+	public PageController(StudentService service) {
 		this.service = service;
 	}
 	
 	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("students", service.getAllStudents());
-		model.addAttribute("newStudent", new Student());
-		
+	public String home() {
 		return "index";
 	}
 	
-	@PostMapping("/addStudent")
-	public String addStudent(@ModelAttribute Student newStudent, Model model) {
-		service.addStudent(newStudent);
-		return index(model);
-	}
+//	@GetMapping("/")
+//	public String index(Model model) {
+//		model.addAttribute("students", service.getAllStudents());
+//		model.addAttribute("newStudent", new Student());
+//		
+//		return "index";
+//	}
+//	
+//	@PostMapping("/addStudent")
+//	public String addStudent(@ModelAttribute Student newStudent, Model model) {
+//		service.addStudent(newStudent);
+//		return index(model);
+//	}
 
 }
